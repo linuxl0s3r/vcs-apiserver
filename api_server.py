a0393f12e21ca54f2513fca7da4f1dc6058729b4
Ubuntu <ubuntu@ip-172-31-9-213.us-west-2.compute.internal>
2017-12-17 08:19:10 +0000

    First commit

---
+++ api_server.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+
+import logging
+#import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, Text, DateTime, create_engine, inspect
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import *
+
+import json
+
+# REST Client
+import requests
+from requests.auth import HTTPBasicAuth
+
+from flask import Flask, render_template, jsonify, flash, request
+app = Flask(__name__)
+app.debug=True
+
+with open('mysql.json') as json_data:
+    data=json.load(json_data)
+    mysqlId=data['id']
+    mysqlPassword=data['password']
+#    print mysqlId, mysqlPassword
+
+
+Base = declarative_base()
+
+to_conn_str = 'mysql://%s:%s@fitcyclecustomers.cy4b7ufzt54x.us-west-2.rds.amazonaws.com:3306/prospect'%(mysqlId,mysqlPassword)
+eng_to = create_engine(to_conn_str, echo=False)
+meta=MetaData(eng_to)
+
+
+polls_prospect = Table('polls_prospect', meta, autoload=True, autoload_with=eng_to)
+
+def getPollsProspect():
+
+    PPTable = []
+    PPRow = {}
+
+    q = select([polls_prospect.c.id, polls_prospect.c.firstname, polls_prospect.c.lastname, polls_prospect.c.email]) 
+
+    results = q.execute()
+
+    for row in results:
+
+        PPRow['id'] = int(row.id)
+        PPRow['firstname'] = row.firstname
+        PPRow['lastname'] = row.lastname
+        PPRow['email'] = row.email
+
+        PPTable.append(PPRow)
+
+        PPRow = {}
+
+    
+ #   print "json", calljson
+
+    return PPTable
+
+@app.route('/api/v1.0/signups', methods=['GET'])
+def get_tasks():
+    PPTable = getPollsProspect()
+    return jsonify({'polls_prospect': PPTable})
+
+
+@app.route('/')
+def hello_world(name=None):
+	return render_template('hello.html') 
+
+
+if __name__ == '__main__':
+
+    app.run(host='172.31.9.213', port=5000)
+
