b9efb08b33f3ae9da0972819d84c8ae6123e18b1
bshetti <billshetti@gmail.com>
2018-05-23 01:51:33 +0000

    added logging format

--- api_server_backup.py
+++ api_server_backup.py
@@ -8,24 +8,6 @@ from sqlalchemy import Column, Integer, String, Float, Text, DateTime, create_en
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import *
 from statsd import StatsClient
-from logging.config import dictConfig
-
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi']
-    }
-})
-
 
 import json
 
